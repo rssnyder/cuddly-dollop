5f03cffbb1a6eeea25e516cf482910bf4233177b
Riley Snyder <riley.snyder@harness.io>
2023-08-23 08:47:31 -0500

    log to file

--- cloud_connectors.py
+++ cloud_connectors.py
@@ -1,5 +1,6 @@
 from csv import reader
 from sys import argv
+from datetime import datetime
 
 from common import (
     CloudAccount,
@@ -10,6 +11,8 @@ from common import (
 
 
 if __name__ == "__main__":
+    current_date = datetime.now()
+
     # program arguments
     if len(argv) < 3:
         print(
@@ -28,53 +31,57 @@ if __name__ == "__main__":
             datareader = reader(cc_data)
             next(datareader)
 
-            for row in datareader:
-                # create instance of account with given data
-                account = CloudAccount(
-                    row[0],
-                    row[1],
-                    row[2],
-                    row[3],
-                    row[4],
-                    row[5],
-                    row[6],
-                    row[7],
-                )
+            with open(
+                f"cloud_connectors{current_date.strftime('%Y-%m-%d_%H:%M:%S')}.txt", "w"
+            ) as file:
+                for row in datareader:
+                    # create instance of account with given data
+                    account = CloudAccount(
+                        row[0],
+                        row[1],
+                        row[2],
+                        row[3],
+                        row[4],
+                        row[5],
+                        row[6],
+                        row[7],
+                    )
 
-                # print(account.identifier)
-                if account.payer_id == account.identifier:
-                    print("CONNECTOR:skipped\t\tpayer account")
+                    # print(account.identifier)
+                    if account.payer_id == account.identifier:
+                        print("CONNECTOR:skipped\t\tpayer account")
 
-                if account.cloud == "aws":
-                    if account.payer_id == "423844416462":
-                        print("CONNECTOR:skipped\t\tlegacy account")
-                        continue
+                    if account.cloud == "aws":
+                        if account.payer_id == "423844416462":
+                            print("CONNECTOR:skipped\t\tlegacy account")
+                            continue
 
-                    if not get_aws_account_cost(account.identifier):
-                        print(
-                            f"CONNECTOR:skipped\t\tno account cost last 90d {account.identifier}"
-                        )
+                        if not get_aws_account_cost(account.identifier):
+                            print(
+                                f"CONNECTOR:skipped\t\tno account cost last 90d {account.identifier}"
+                            )
 
-                elif account.cloud == "azure":
-                    if not get_azure_subscription_cost(account.identifier):
-                        print(
-                            f"CONNECTOR:skipped\t\tno account cost last 90d {account.identifier}"
-                        )
-                        continue
+                    elif account.cloud == "azure":
+                        if not get_azure_subscription_cost(account.identifier):
+                            print(
+                                f"CONNECTOR:skipped\t\tno account cost last 90d {account.identifier}"
+                            )
+                            continue
 
-                elif account.cloud == "gcp":
-                    if not get_gcp_project_cost(account.identifier):
-                        print(
-                            f"CONNECTOR:skipped\t\tno account cost last 90d {account.identifier}"
-                        )
-                        continue
+                    elif account.cloud == "gcp":
+                        if not get_gcp_project_cost(account.identifier):
+                            print(
+                                f"CONNECTOR:skipped\t\tno account cost last 90d {account.identifier}"
+                            )
+                            continue
 
-                print(
-                    account.create_connector(
+                    result = account.create_connector(
                         cross_account_role,
                         tenant_id,
                         service_account_email,
                         dry_run=False,
                     )
-                )
-                # print(account.delete_connector())
+
+                    file.write(result + "\n")
+                    print(result)
+                    # print(account.delete_connector())
