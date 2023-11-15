2373161a546f7f5703e363014d871a9089855dae
Riley Snyder <riley.snyder@harness.io>
2023-11-15 10:56:11 -0600

    legacy; add apm_cost_category_xlsx.py

---
+++ apm_cost_category_xlsx.py
@@ -0,0 +1,72 @@
+from csv import reader
+from os import getenv
+from sys import argv, exit
+from time import sleep
+
+import pandas as pd
+import numpy as np
+
+from common import CloudAccount, CostCatagory
+
+
+if __name__ == "__main__":
+    
+    # program arguments
+    if len(argv) < 2:
+        print(f"usage: {argv[0]} [xlsx]")
+        exit(1)
+
+    xlsx = argv[1]
+
+    # core_tags = CostCatagory(core_tags_cc_name)
+
+    sheet = pd.read_excel(xlsx)
+
+    cost_catagories = {}
+    {
+        "AppOwner": {
+            "Yermalkar, Sanjay": [
+                "value"
+            ]
+        }
+    }
+    
+    # create cc for all columns
+    for column in sheet.columns[1:]:
+        cost_catagories.update({column.replace(" ", ""): {}})
+
+    # for every row in the excel
+    for _, row in sheet.iterrows():
+
+        # get the column names
+        columns = row.keys()
+
+        # for every column
+        for column in columns[1:]:
+
+            # no spaces in cc names
+            cc_name = column.replace(" ", "")
+            bucket_name = str(row[column])
+            if bucket_name == "nan":
+                bucket_name = "No Entry"
+
+            # add tag to cc/bucket
+            if bucket_name in cost_catagories[cc_name]:
+                cost_catagories[cc_name][bucket_name].append(row.iloc[0])
+            else:
+                cost_catagories[cc_name].update({bucket_name: [row.iloc[0]]})
+
+    for cc in cost_catagories:
+        print("\n\n==============", cc)
+        for bucket in cost_catagories[cc]:
+            print("\t", bucket, cost_catagories[cc][bucket])
+
+        print(f"\n\n\n\n==============\nPlease see the above for the new {cc} cost catagory")
+        val = input("Should we apply this update? (yes/no): ")
+        if val == "yes":
+            # print(unit_group_cc_name, unit_groups.update())
+            print("cool")
+
+        val = input("Should we continue? (yes/no): ")
+        if val != "yes":
+            exit(0)
