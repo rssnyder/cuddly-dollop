3ec9ea18f29b857d79c20e30306e25d5f6e6dbdd
Riley Snyder <riley.snyder@harness.io>
2024-07-02 09:29:23 -0500

    dedup cluster lables

--- costcenter_cost_category_xlsx.py
+++ costcenter_cost_category_xlsx.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
                                 "identifierName": "label",
                             },
                             "viewOperator": "IN",
-                            "values": allValues,
+                            "values": list(set(allValues)),
                         }
                     ]
                 }
